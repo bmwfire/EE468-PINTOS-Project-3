dcbb28625acebc51a016535874e2bf86e52404d1
Brandon Wong <bwong7@hawaii.edu>
2018-12-08 00:20:11 -1000

    Removed struct process

--- pintos/src/userprog/process.h
+++ pintos/src/userprog/process.h
@@ -16,17 +16,17 @@ enum load_status
   };
 
 /* A structure storing infomation about a child process. */
-struct process
-  {
-    struct list_elem elem;        /* List element for child processes list. */
-    pid_t pid;                    /* The process/thread id. */
-    bool is_alive;                /* Whether the process has exited or not. */
-    bool is_waited;               /* Whether the process has been or is waited upon. */
-    int exit_status;              /* The exit status of the process. */
-    enum load_status load_status; /* The load status of the file being executed by the process. */
-    struct semaphore wait;        /* Used to wait for a process to exit before returning it's exit status. */
-    struct semaphore load;        /* Used to wait for the file being executed to load (or fail to load). */
-  };
+//struct process
+//  {
+//    struct list_elem elem;        /* List element for child processes list. */
+//    pid_t pid;                    /* The process/thread id. */
+//    bool is_alive;                /* Whether the process has exited or not. */
+//    bool is_waited;               /* Whether the process has been or is waited upon. */
+//    int exit_status;              /* The exit status of the process. */
+//    enum load_status load_status; /* The load status of the file being executed by the process. */
+//    struct semaphore wait;        /* Used to wait for a process to exit before returning it's exit status. */
+//    struct semaphore load;        /* Used to wait for the file being executed to load (or fail to load). */
+//  };
 
 tid_t process_execute (const char *);
 int process_wait (tid_t);
