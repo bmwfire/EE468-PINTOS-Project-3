7ff64fc1f47ca88587428cc3713c16e115db475c
Brandon Wong <bwong7@hawaii.edu>
2018-12-08 02:08:03 -1000

    Progress with swap

--- pintos/src/threads/init.c
+++ pintos/src/threads/init.c
@@ -22,6 +22,7 @@
 #include "threads/palloc.h"
 #include "threads/pte.h"
 #include "threads/thread.h"
+#include "vm/swap.h"
 #ifdef USERPROG
 #include "userprog/process.h"
 #include "userprog/exception.h"
@@ -127,6 +128,11 @@ main (void)
   ide_init ();
   locate_block_devices ();
   filesys_init (format_filesys);
+#endif
+
+#ifdef VM
+  locate_block_devices ();
+  vm_swap_init();
 #endif
 
   printf ("Boot complete.\n");
