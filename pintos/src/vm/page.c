7f0819921b30b512e08b968bbef34adaeb1985b5
Brandon Wong <bwong7@hawaii.edu>
2018-12-08 04:48:06 -1000

    New changes 6

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -49,21 +49,29 @@ bool load_page(struct sup_page_entry *spe){
 bool load_file_page(struct sup_page_entry *spe){
   struct thread *curr = thread_current ();
 
-  file_seek (spe->data.file, spe->data.offset);//look in file at offset
+  file_seek (spe->data.file_page.file, spe->data.file_page.offset);//look in
+  // file at
+  // offset
 
   uint8_t *page = vm_get_frame(PAL_USER);//allocate a page
   if(page == NULL){//allocation must have failed, load unsuccessful
     return false;
   }
 
-  if(file_read(spe->data.file, page, spe->data.read_bytes) != (int) spe->data.read_bytes){//check if file is at certain offset. clear frame and return fail if it is
+  if(file_read(spe->data.file_page.file, page, spe->data.file_page
+      .read_bytes) != (int)
+      spe->data.file_page.read_bytes){//check if file is at certain offset.
+    // clear frame and return fail if it is
     vm_free_frame(page);
     return false;
   }
 
-  memset (page + spe->data.read_bytes, 0, spe->data.zero_bytes);//set the zero bytes of the page to 0
+  memset (page + spe->data.file_page.read_bytes, 0, spe->data.file_page
+      .zero_bytes);
+  //set the zero bytes of the page to 0
 
-  if(!pagedir_set_page(curr->pagedir, spe->user_vaddr, page, spe->data.writable)){//attempt to map page to physical
+  if(!pagedir_set_page(curr->pagedir, spe->user_vaddr, page, spe->data
+      .file_page.writable)){//attempt to map page to physical
     vm_free_frame(page);//if failed to map, then free frame and return unsuccessful
     return false;
   }
